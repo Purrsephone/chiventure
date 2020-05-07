d50da4c6bb732a1f42d98302efd03a8feda121a7
Sophie Veys <sophiev@cs.uchicago.edu>
2020-05-06 22:28:16 -0400

    meow

--- src/battle/sandbox/players_stats.c
+++ src/battle/sandbox/players_stats.c
@@ -4,7 +4,7 @@
 struct stats_t
 {
 	int speed;
-	int stength;
+	int strength;
 	int dexterity;
 	int hp;
 	int xp;
@@ -26,17 +26,26 @@ struct player_t
 
 void create_test_stats()
 {
-	*test_stats = malloc(sizeof(stats_t));
-	test_stats.speed = 30;
-	test_stats.strength = 80;
-	test_stats.dexterity = 20;
-	test_stats.hp = 100;
-	test_stats.xp = 180;
+	struct stats_t *test_stats = malloc(sizeof(stats_t));
+	test_stats->speed = 30;
+	test_stats->strength = 80;
+	test_stats->dexterity = 20;
+	test_stats->hp = 100;
+	test_stats->xp = 180;
+}
+
+*list_t create_test_inventory()
+{
+	list_t *inv_list = malloc(sizeof(list_t));
+	
 }
 
 void create_test_player()
 {
-
+	struct player_t *test_player = malloc(sizeof(player_t));
+	test_player->cl = 3;
+//	test_player->inventory = NULL;
+//	test_player->armor = NULL;
 }
 
 int main()
